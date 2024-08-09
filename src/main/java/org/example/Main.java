56b1f3fbcf3357a581eba438768a598eb12f8af1
aleksey <meansnecessary@gmail.com>
2024-08-09 13:25:28 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int balancesumm;
+        balancesumm = 2_000_000_000;
+        System.out.printf("balancesumm");
+        int transfersumm = 500_000_000;
+        System.out.println(transfersumm);
+        int totalsumm = balancesumm + transfersumm;
+        System.out.println(totalsumm);
+    }
+}
